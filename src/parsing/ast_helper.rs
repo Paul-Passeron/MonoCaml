9997b80af17a128eaa6ab9f48f10faee3198c9ad
Paul-Passeron <paul.passeron2@gmail.com>
2025-12-11 19:18:02 +0100

    Add varify_constructors method to CoreType

--- src/parsing/ast_helper.rs
+++ src/parsing/ast_helper.rs
@@ -9,10 +9,13 @@ use crate::{
             Attribute, Attributes, Constant, ConstantDesc, CoreType, CoreTypeDesc, Extension,
             ObjectField, PackageType, Payload, RowField,
         },
+        syntaxerr,
     },
     platform::NativeInt,
 };
 use lazy_static::lazy_static;
+
+use super::parsetree::{ObjectFieldDesc, RowFieldDesc};
 
 pub type LId = Loc<LongIdent>;
 pub type Str = Loc<String>;
@@ -231,5 +234,180 @@ impl CoreType {
                 self,
             ),
         }
+    }
+
+    pub fn varify_constructors(self, var_names: &Vec<Str>) -> Result<Self, syntaxerr::Error> {
+        CoreTypeHelper::new(&var_names).loopfun(&self)
+    }
+
+    pub fn package_type(
+        loc: Option<Location>,
+        attrs: Option<Attrs>,
+        p: LId,
+        c: Vec<(LId, CoreType)>,
+    ) -> PackageType {
+        PackageType {
+            path: p,
+            constraints: c,
+            loc: loc.unwrap_or(get_default_loc()),
+            attrs: attrs.unwrap_or_default(),
+        }
+    }
+}
+
+struct CoreTypeHelper<'a> {
+    vars: &'a Vec<Str>,
+}
+
+impl<'a> CoreTypeHelper<'a> {
+    pub fn new(vars: &'a Vec<Str>) -> Self {
+        Self { vars }
+    }
+
+    pub fn check_variable(&self, var: &String, loc: &Location) -> Result<(), syntaxerr::Error> {
+        if self.mem(var) {
+            Err(syntaxerr::Error::VariableInScope(loc.clone(), var.clone()))
+        } else {
+            Ok(())
+        }
+    }
+
+    pub fn mem(&self, v: &String) -> bool {
+        self.vars.iter().find(|Loc { txt, loc }| txt == v).is_some()
+    }
+}
+
+trait CoreTypeLoopHelper<T> {
+    fn loopfun(&self, x: &T) -> Result<T, syntaxerr::Error>;
+}
+
+impl<'a, T> CoreTypeLoopHelper<Vec<T>> for CoreTypeHelper<'a>
+where
+    CoreTypeHelper<'a>: CoreTypeLoopHelper<T>,
+{
+    fn loopfun(&self, x: &Vec<T>) -> Result<Vec<T>, syntaxerr::Error> {
+        x.iter().map(|x| self.loopfun(x)).collect()
+    }
+}
+
+impl<'a, T> CoreTypeLoopHelper<Box<T>> for CoreTypeHelper<'a>
+where
+    CoreTypeHelper<'a>: CoreTypeLoopHelper<T>,
+{
+    fn loopfun(&self, x: &Box<T>) -> Result<Box<T>, syntaxerr::Error> {
+        Ok(Box::new(self.loopfun(x)?))
+    }
+}
+
+impl<'a, A, B> CoreTypeLoopHelper<(B, A)> for CoreTypeHelper<'a>
+where
+    CoreTypeHelper<'a>: CoreTypeLoopHelper<A>,
+    B: Clone,
+{
+    fn loopfun(&self, (l, x): &(B, A)) -> Result<(B, A), syntaxerr::Error> {
+        Ok((l.clone(), self.loopfun(x)?))
+    }
+}
+
+impl<'a> CoreTypeLoopHelper<CoreType> for CoreTypeHelper<'a> {
+    fn loopfun(&self, ty: &CoreType) -> Result<CoreType, syntaxerr::Error> {
+        let desc = match &ty.type_desc {
+            CoreTypeDesc::Any => CoreTypeDesc::Any,
+            CoreTypeDesc::Var(x) => {
+                self.check_variable(x, &ty.loc)?;
+                CoreTypeDesc::Var(x.clone())
+            }
+            CoreTypeDesc::Arrow(arglabel, ty1, ty2) => {
+                let new_ty1 = self.loopfun(ty1)?;
+                let new_ty2 = self.loopfun(ty2)?;
+                CoreTypeDesc::Arrow(arglabel.clone(), new_ty1, new_ty2)
+            }
+            CoreTypeDesc::Tuple(tys) => CoreTypeDesc::Tuple(self.loopfun(tys)?),
+            CoreTypeDesc::Constr(
+                Loc {
+                    txt: LongIdent::Ident(label),
+                    ..
+                },
+                lst,
+            ) if lst.is_empty() && self.mem(&label) => CoreTypeDesc::Var(label.clone()),
+            CoreTypeDesc::Constr(longident, lst) => {
+                CoreTypeDesc::Constr(longident.clone(), self.loopfun(lst)?)
+            }
+            CoreTypeDesc::Object(fields, flag) => {
+                CoreTypeDesc::Object(self.loopfun(fields)?, *flag)
+            }
+            CoreTypeDesc::Class(longident, lst) => {
+                CoreTypeDesc::Class(longident.clone(), self.loopfun(lst)?)
+            }
+            CoreTypeDesc::Alias(core_type, alias) => {
+                self.check_variable(&alias.txt, &alias.loc)?;
+                CoreTypeDesc::Alias(self.loopfun(core_type)?, alias.clone())
+            }
+            CoreTypeDesc::Variant(row_field_list, flag, lbl_lst_option) => {
+                CoreTypeDesc::Variant(self.loopfun(row_field_list)?, *flag, lbl_lst_option.clone())
+            }
+            CoreTypeDesc::Poly(string_lst, core_type) => {
+                string_lst
+                    .iter()
+                    .try_for_each(|Loc { txt, loc }| self.check_variable(txt, loc))?;
+                CoreTypeDesc::Poly(string_lst.clone(), self.loopfun(core_type)?)
+            }
+            CoreTypeDesc::Package(ptyp) => CoreTypeDesc::Package(self.loopfun(ptyp)?),
+            CoreTypeDesc::Open(mod_ident, core_type) => {
+                CoreTypeDesc::Open(mod_ident.clone(), self.loopfun(core_type)?)
+            }
+            CoreTypeDesc::Extension(ext) => CoreTypeDesc::Extension(ext.clone()),
+        };
+        Ok(CoreType {
+            type_desc: desc,
+            loc: ty.loc.clone(),
+            loc_stack: ty.loc_stack.clone(),
+            attributes: ty.attributes.clone(),
+        })
+    }
+}
+
+impl<'a> CoreTypeLoopHelper<RowField> for CoreTypeHelper<'a> {
+    fn loopfun(&self, field: &RowField) -> Result<RowField, syntaxerr::Error> {
+        let desc = match &field.desc {
+            RowFieldDesc::Tag(label, flag, core_types) => {
+                RowFieldDesc::Tag(label.clone(), *flag, self.loopfun(core_types)?)
+            }
+            RowFieldDesc::Inherit(core_type) => RowFieldDesc::Inherit(self.loopfun(core_type)?),
+        };
+        Ok(RowField {
+            desc,
+            loc: field.loc.clone(),
+            attributes: field.attributes.clone(),
+        })
+    }
+}
+
+impl<'a> CoreTypeLoopHelper<ObjectField> for CoreTypeHelper<'a> {
+    fn loopfun(&self, field: &ObjectField) -> Result<ObjectField, syntaxerr::Error> {
+        let desc = match &field.desc {
+            ObjectFieldDesc::Tag(loc, core_type) => {
+                ObjectFieldDesc::Tag(loc.clone(), self.loopfun(core_type)?)
+            }
+            ObjectFieldDesc::Inherit(core_type) => {
+                ObjectFieldDesc::Inherit(self.loopfun(core_type)?)
+            }
+        };
+        Ok(ObjectField {
+            desc,
+            loc: field.loc.clone(),
+            attributes: field.attributes.clone(),
+        })
+    }
+}
+
+impl<'a> CoreTypeLoopHelper<PackageType> for CoreTypeHelper<'a> {
+    fn loopfun(&self, ptyp: &PackageType) -> Result<PackageType, syntaxerr::Error> {
+        Ok(PackageType {
+            path: ptyp.path.clone(),
+            loc: ptyp.loc.clone(),
+            attrs: ptyp.attrs.clone(),
+            constraints: self.loopfun(&ptyp.constraints)?,
+        })
     }
 }
