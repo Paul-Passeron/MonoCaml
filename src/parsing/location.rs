726f00c9854aab8c542dd677f347d1ceabed43cf
Paul-Passeron <paul.passeron2@gmail.com>
2025-12-11 02:01:14 +0100

    Add convenience methods for handling none Location

--- src/parsing/location.rs
+++ src/parsing/location.rs
@@ -6,4 +6,12 @@ impl Location {
     pub fn in_file<T: ToString>(file_name: T) -> Self {
         warnings::Location::ghost_loc_in_file(file_name)
     }
+
+    pub fn none() -> Self {
+        Self::in_file("_none_")
+    }
+
+    pub fn is_none(&self) -> bool {
+        self == &Self::none()
+    }
 }
