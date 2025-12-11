4b6f5d349e7b23f49e8f0ef560846cc1184b3662
Paul-Passeron <paul.passeron2@gmail.com>
2025-12-12 00:01:20 +0100

    Add text attribute method to DocString struct

--- src/parsing/docstring.rs
+++ src/parsing/docstring.rs
@@ -1,4 +1,9 @@
-use crate::parsing::location::Location;
+use crate::parsing::{
+    asttypes::Loc,
+    location::Location,
+    parsetree::{Attribute, Constant, Expression, Payload, StructureItem, StructureItemDesc},
+};
+use lazy_static::lazy_static;
 
 pub enum DSAttached {
     Unattached,
@@ -13,8 +18,31 @@ pub enum DSAssociated {
 }
 
 pub struct DocString<'a> {
-    body: &'a str,
-    loc: Location,
-    attached: DSAttached,
-    associated: DSAssociated,
+    pub body: &'a str,
+    pub loc: Location,
+    pub attached: DSAttached,
+    pub associated: DSAssociated,
+}
+
+lazy_static! {
+    static ref TEXT_LOC: Loc<String> = {
+        Loc {
+            txt: format!("ocaml.text"),
+            loc: Location::none(),
+        }
+    };
+}
+
+impl DocString<'_> {
+    pub fn text_attr(&self) -> Attribute {
+        let loc = self.loc.clone();
+        let c = Constant::string(None, Some(loc.clone()), self.body.into());
+        let exp = Expression::constant(Some(loc.clone()), None, c);
+        let item = StructureItem {
+            desc: StructureItemDesc::Eval(Box::new(exp), vec![]),
+            loc: loc.clone(),
+        };
+
+        Attribute::mk(Some(loc), TEXT_LOC.clone(), Payload::Str(vec![item]))
+    }
 }
