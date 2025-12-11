f4939985094e462e537d4ee3b0da3430e4f29a51
Paul-Passeron <paul.passeron2@gmail.com>
2025-12-11 02:00:58 +0100

    Add ToString and Clone/Copy traits to NativeInt

--- src/platform/mod.rs
+++ src/platform/mod.rs
@@ -1,5 +1,21 @@
-pub trait NativeInt {}
+pub trait NativeInt: ToString + Clone + Copy {}
+
+#[derive(Clone, Copy)]
 pub struct Native32(pub i32);
+
+#[derive(Clone, Copy)]
 pub struct Native64(pub i64);
+
 impl NativeInt for Native32 {}
+impl ToString for Native32 {
+    fn to_string(&self) -> String {
+        self.0.to_string()
+    }
+}
+
 impl NativeInt for Native64 {}
+impl ToString for Native64 {
+    fn to_string(&self) -> String {
+        self.0.to_string()
+    }
+}
