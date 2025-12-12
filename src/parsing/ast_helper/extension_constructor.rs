3e962286aba3f178314913afdafc7cf783a78747
Paul-Passeron <paul.passeron2@gmail.com>
2025-12-12 19:15:09 +0100

    Add extension constructor creation methods

--- src/parsing/ast_helper/extension_constructor.rs
+++ src/parsing/ast_helper/extension_constructor.rs
@@ -1,9 +1,10 @@
 use crate::parsing::{
-    ast_helper::{get_default_loc, Str},
-    asttypes::MutableFlag,
+    ast_helper::{get_default_loc, LId, Str},
     docstring::{add_info_attrs, get_empty_info, Info},
     location::Location,
-    parsetree::{Attributes, ExtensionConstructor, ExtensionConstructorKind},
+    parsetree::{
+        Attributes, ConstructorArguments, CoreType, ExtensionConstructor, ExtensionConstructorKind,
+    },
 };
 
 impl ExtensionConstructor {
@@ -11,17 +12,55 @@ impl ExtensionConstructor {
         loc: Option<Location>,
         attrs: Option<Attributes>,
         infos: Option<Info>,
-        mut_flag: Option<MutableFlag>,
         name: Str,
         kind: ExtensionConstructorKind,
     ) -> Self {
         let loc = loc.unwrap_or_else(get_default_loc);
         let mut attributes = attrs.unwrap_or_default();
         add_info_attrs(infos.unwrap_or_else(get_empty_info), &mut attributes);
-        let mutable = mut_flag.unwrap_or(MutableFlag::Immutable);
         Self {
             name,
             kind,
+            attributes,
+            loc,
+        }
+    }
+
+    pub fn decl(
+        loc: Option<Location>,
+        attrs: Option<Attributes>,
+        infos: Option<Info>,
+        vars: Option<Vec<Str>>,
+        args: Option<ConstructorArguments>,
+        res: Option<CoreType>,
+        name: Str,
+    ) -> Self {
+        let loc = loc.unwrap_or_else(get_default_loc);
+        let mut attributes = attrs.unwrap_or_default();
+        add_info_attrs(infos.unwrap_or_else(get_empty_info), &mut attributes);
+        let vars = vars.unwrap_or_default();
+        let args = args.unwrap_or(ConstructorArguments::Tuple(vec![]));
+        Self {
+            name,
+            kind: ExtensionConstructorKind::Decl(vars, args, res),
+            attributes,
+            loc,
+        }
+    }
+
+    pub fn rebind(
+        loc: Option<Location>,
+        attrs: Option<Attributes>,
+        info: Option<Info>,
+        name: Str,
+        lid: LId,
+    ) -> Self {
+        let loc = loc.unwrap_or_else(get_default_loc);
+        let mut attributes = attrs.unwrap_or_default();
+        add_info_attrs(info.unwrap_or_else(get_empty_info), &mut attributes);
+        Self {
+            name,
+            kind: ExtensionConstructorKind::Rebind(lid),
             attributes,
             loc,
         }
