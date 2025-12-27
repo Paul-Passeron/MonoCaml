e44d47267788e728e5a185e91862ea2d79e4ca3a
Paul-Passeron <paul.passeron2@gmail.com>
2025-12-28 00:16:46 +0100

    Refactor CFG module with structural updates

--- src/cfg/mod.rs
+++ src/cfg/mod.rs
@@ -1,10 +1,16 @@
-use std::collections::{HashMap, HashSet};
+use std::{
+    collections::{HashMap, HashSet},
+    hash::Hash,
+};
 
 use crate::{
     cfg::var::{CfgGlobal, CfgGlobalUse, CfgVar, CfgVarUse, VarKind},
     helpers::unique::{Unique, Use},
 };
+pub mod builder;
+mod display;
 mod extract;
+mod get_type;
 mod token;
 mod var;
 
@@ -62,43 +68,36 @@ pub enum Expr {
     Div(Value, Value),
 
     // This expects a closure as first arg
-    Call {
-        closure: Value,
-        arg: Value,
-    },
+    Call { closure: Value, arg: Value },
 
-    NativeCall {
-        funptr: FunNameUse,
-        args: Vec<Value>,
-    },
+    NativeCall { fun: String, args: Vec<Value> },
 
-    GetElementPtr {
-        ptr: Value,
-        index: usize,
-    },
+    GetElementPtr { ptr: Value, index: usize },
 
-    Load {
-        ptr: Value,
-        ty: Ty,
-    },
+    Extract { value: Value, index: usize },
+
+    Load { ptr: Value, ty: Ty },
 
     Struct(Vec<Value>),
 }
 
+#[derive(Debug, Clone, PartialEq, PartialOrd)]
 pub enum Ty {
     Int,
     String,
+    Void,
     Ptr(Box<Self>),
     Struct(Vec<Self>),
+    FunPtr(Sig),
 }
 
 pub enum Instr<V: VarKind> {
     Assign(V, Expr),
-    Store(Value, Ty, Value),
+    Store { ptr: Value, value: Value },
 }
 
 impl Instr<CfgVar> {
-    pub fn into_use<T, Z>(self) -> (Option<CfgVar>, Instr<T>)
+    pub fn into_use<T>(self) -> (Option<CfgVar>, Instr<T>)
     where
         for<'a> T: VarKind + From<&'a CfgVar>,
     {
@@ -107,7 +106,7 @@ impl Instr<CfgVar> {
                 let new_var = (&var).into();
                 (Some(var), Instr::Assign(new_var, expr))
             }
-            Instr::Store(ptr, ty, val) => (None, Instr::Store(ptr, ty, val)),
+            Instr::Store { ptr, value } => (None, Instr::Store { ptr, value }),
         }
     }
 }
@@ -133,19 +132,36 @@ pub struct Cfg {
     blocks: Vec<BasicBlock>,
 }
 
+#[derive(Debug, Clone, PartialEq, PartialOrd)]
 pub struct Sig {
     params: Vec<Ty>,
-    ret: Ty,
+    ret: Box<Ty>,
 }
 
 pub struct Func {
     name: FunName,
-    sig: Sig,
-    cfg: Cfg,
+    params: Vec<(CfgVar, Ty)>,
+    ret_ty: Ty,
+    cfg: Option<Cfg>,
+}
+
+impl PartialEq for Func {
+    fn eq(&self, other: &Self) -> bool {
+        self.name == other.name
+    }
+}
+
+impl Eq for Func {}
+
+impl Hash for Func {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.name.hash(state);
+    }
 }
 
 pub struct Program {
     globals: HashMap<CfgGlobal, Const>,
+    natives: HashMap<String, FunNameUse>,
     funcs: HashSet<Func>,
 }
 
@@ -153,4 +169,5 @@ pub struct TyCtx {
     pub sigs: HashMap<FunNameUse, Sig>,
     pub globals: HashMap<CfgGlobalUse, Ty>,
     pub vars: HashMap<CfgVarUse, Ty>,
+    pub natives: HashMap<String, FunNameUse>,
 }
