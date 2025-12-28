9a6bca2f52da312028f02df283e95e9b3ccac947
Paul-Passeron <paul.passeron2@gmail.com>
2025-12-29 00:01:13 +0100

    Add test case for higher-order function composition

--- src/main.rs
+++ src/main.rs
@@ -49,8 +49,42 @@ fn test_ast2() -> Ast {
     )
 }
 
+fn test_ast_3() -> Ast {
+    let x = Var::fresh();
+    let y = Var::fresh();
+    let z = Var::fresh();
+    let wrapper = Ast::lambda(
+        AstTyped::new(x, AstTy::Int),
+        Ast::lambda(
+            AstTyped::new(y, AstTy::Int),
+            Ast::lambda(
+                AstTyped::new(z, AstTy::fun(AstTy::Int, AstTy::Int)),
+                Ast::app(
+                    Ast::app(
+                        Ast::Native("add".into()),
+                        Ast::app(Ast::Var(z), Ast::Var(x)),
+                    ),
+                    Ast::app(Ast::Var(z), Ast::Var(y)),
+                ),
+            ),
+        ),
+    );
+
+    let a = Var::fresh();
+
+    let print_ret = Ast::lambda(
+        AstTyped::new(a, AstTy::Int),
+        Ast::seq(Ast::app(Ast::native("print_int"), Ast::Var(a)), Ast::Var(a)),
+    );
+
+    Ast::app(
+        Ast::app(Ast::app(wrapper, Ast::Int(4)), Ast::Int(6)),
+        print_ret,
+    )
+}
+
 fn main() {
-    let ast = test_ast2();
+    let ast = test_ast_3();
     println!("{ast}\n");
     let free_vars = ast.free_vars();
     for var in free_vars {
