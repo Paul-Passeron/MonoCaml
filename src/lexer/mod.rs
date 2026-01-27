396c34b4619e4b71c166f2f66b8c84b8289b7365
Paul-Passeron <paul.passeron2@gmail.com>
2026-01-27 16:55:46 +0100

    Implement lexer with token parsing and comment handling

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -1,6 +1,331 @@
+use std::marker::PhantomData;
+
+use crate::{
+    lexer::token::{Token, TokenKind},
+    session::Session,
+    source_manager::{FileId, SourceManager, loc::Loc},
+};
+
 pub mod interner;
 pub mod token;
 
-pub struct Lexer<'src> {
-    pub contents: &'src str,
+pub struct Lexer<'session> {
+    pub contents: String,
+    pub file: FileId,
+    pub pos: usize,
+    _session: PhantomData<&'session ()>,
+}
+
+pub enum LexingError {
+    EOF,
+    UnexpectedChar(Loc),
+    UnmatchedCommentStart(Loc),
+    NotAnOperator(Loc),
+}
+
+pub type LexRes = Result<Token, LexingError>;
+
+fn is_identifier_start(c: char) -> bool {
+    c.is_alphabetic() || c == '_'
+}
+
+fn is_identifier_continue(c: char) -> bool {
+    is_identifier_start(c) || c.is_numeric()
+}
+
+impl<'session> Lexer<'session> {
+    pub fn new(sm: &SourceManager, file: FileId) -> Self {
+        Self {
+            contents: sm.get_file(file).contents.clone(),
+            file,
+            pos: 0,
+            _session: PhantomData,
+        }
+    }
+
+    fn loc(&self) -> Loc {
+        Loc::new(self.file, self.pos)
+    }
+
+    fn peek_n(&self, n: usize) -> Option<char> {
+        if self.pos + n >= self.contents.len() {
+            return None;
+        }
+        let c = self.contents[self.pos + n..]
+            .chars()
+            .next()
+            .unwrap_or_default();
+        Some(c)
+    }
+
+    fn peek(&self) -> Option<char> {
+        self.peek_n(0)
+    }
+
+    fn advance(&mut self) -> Option<char> {
+        let c = self.peek()?;
+        self.pos += 1;
+        Some(c)
+    }
+
+    fn skip_whitespace(&mut self) {
+        while let Some(c) = self.peek()
+            && c.is_whitespace()
+        {
+            let _ = self.advance();
+        }
+    }
+
+    fn is_comment_start(&self) -> bool {
+        self.peek_n(0) == Some('(') && self.peek_n(1) == Some('*')
+    }
+
+    fn is_comment_end(&self) -> bool {
+        self.peek_n(0) == Some('*') && self.peek_n(1) == Some(')')
+    }
+
+    fn peek_or_eof(&self) -> Result<char, LexingError> {
+        self.peek().ok_or_else(|| LexingError::EOF)
+    }
+
+    fn lex_comment(&mut self) -> LexRes {
+        let mut s = String::new();
+        let l = self.loc();
+        let mut opens = vec![l];
+
+        if !self.is_comment_start() {
+            return Err(LexingError::UnexpectedChar(self.loc()));
+        }
+        let mut depth = 1;
+        s.push(self.advance().unwrap_or_default());
+        s.push(self.advance().unwrap_or_default());
+        while depth > 0 && self.peek().is_some() {
+            if self.is_comment_start() {
+                opens.push(self.loc());
+                depth += 1;
+                s.push(self.advance().unwrap_or_default());
+                s.push(self.advance().unwrap_or_default());
+            } else if self.is_comment_end() {
+                depth -= 1;
+                opens.pop();
+                s.push(self.advance().unwrap_or_default());
+                s.push(self.advance().unwrap_or_default());
+            } else {
+                s.push(self.advance().unwrap_or_default());
+            }
+        }
+
+        if depth != 0 {
+            return Err(LexingError::UnmatchedCommentStart(opens.pop().unwrap_or(l)));
+        } else {
+        }
+
+        Ok(Token::new(TokenKind::Comment(s), l.span(&self.loc())))
+    }
+
+    fn create_single_char_token(&mut self, kind: TokenKind) -> Token {
+        let l = self.loc();
+        self.advance();
+        let span = l.span(&self.loc());
+        Token::new(kind, span)
+    }
+
+    fn is_operator_char(c: char) -> bool {
+        matches!(
+            c,
+            '!' | '$'
+                | '%'
+                | '&'
+                | '*'
+                | '+'
+                | '-'
+                | '.'
+                | '/'
+                | ':'
+                | '<'
+                | '='
+                | '>'
+                | '?'
+                | '@'
+                | '^'
+                | '|'
+                | '~'
+        )
+    }
+
+    fn is_operator_start(&self, is_first: bool) -> bool {
+        if let Some(c) = self.peek() {
+            match c {
+                '?' | '~' if is_first => self.peek_n(1).map_or(false, Self::is_operator_char),
+                '!' | '$' | '%' | '&' | '*' | '+' | '-' | '.' | '/' | ':' | '<' | '=' | '>'
+                | '@' | '^' | '|' => true,
+                '?' | '~' => true, // valid as continuation
+                _ => false,
+            }
+        } else {
+            false
+        }
+    }
+
+    fn lex_operator(&mut self, session: &mut Session) -> LexRes {
+        if !self.is_operator_start(true) {
+            return Err(LexingError::UnexpectedChar(self.loc()));
+        }
+        let l = self.loc();
+
+        while self.is_operator_start(false) {
+            self.advance();
+        }
+
+        let s = &self.contents[l.offset..self.loc().offset];
+
+        match s {
+            "?" | "~" => {
+                return Err(LexingError::NotAnOperator(l));
+            }
+            // Reserved tokens
+            "+" => Ok(Token::new(TokenKind::Plus, l.span(&self.loc()))),
+            "-" => Ok(Token::new(TokenKind::Minus, l.span(&self.loc()))),
+            "*" => Ok(Token::new(TokenKind::Star, l.span(&self.loc()))),
+            "/" => Ok(Token::new(TokenKind::Div, l.span(&self.loc()))),
+            "!" => Ok(Token::new(TokenKind::Exclam, l.span(&self.loc()))),
+            "=" => Ok(Token::new(TokenKind::Eq, l.span(&self.loc()))),
+            "<>" => Ok(Token::new(TokenKind::NEq, l.span(&self.loc()))),
+            ">" => Ok(Token::new(TokenKind::GT, l.span(&self.loc()))),
+            "<" => Ok(Token::new(TokenKind::LT, l.span(&self.loc()))),
+            ">=" => Ok(Token::new(TokenKind::GEq, l.span(&self.loc()))),
+            "<=" => Ok(Token::new(TokenKind::LEq, l.span(&self.loc()))),
+            "||" => Ok(Token::new(TokenKind::LOr, l.span(&self.loc()))),
+            "::" => Ok(Token::new(TokenKind::Cons, l.span(&self.loc()))),
+            "|" => Ok(Token::new(TokenKind::Pipe, l.span(&self.loc()))),
+            "->" => Ok(Token::new(TokenKind::Arrow, l.span(&self.loc()))),
+            "$" => Ok(Token::new(TokenKind::Pound, l.span(&self.loc()))),
+
+            _ => {
+                let op_symbol = session.intern_symbol(s);
+                Ok(Token::new(TokenKind::Op(op_symbol), l.span(&self.loc())))
+            }
+        }
+    }
+
+    fn lex_identifier(&mut self, session: &mut Session) -> LexRes {
+        if !is_identifier_start(self.peek_or_eof()?) {
+            return Err(LexingError::UnexpectedChar(self.loc()));
+        }
+        let l = self.loc();
+        while let Some(c) = self.peek()
+            && is_identifier_continue(c)
+        {
+            self.advance();
+        }
+
+        let s = &self.contents[l.offset..self.loc().offset];
+        match s {
+            // Keywords
+            "if" => Ok(Token::new(TokenKind::If, l.span(&self.loc()))),
+            "then" => Ok(Token::new(TokenKind::Then, l.span(&self.loc()))),
+            "else" => Ok(Token::new(TokenKind::Else, l.span(&self.loc()))),
+            "for" => Ok(Token::new(TokenKind::For, l.span(&self.loc()))),
+            "while" => Ok(Token::new(TokenKind::While, l.span(&self.loc()))),
+            "begin" => Ok(Token::new(TokenKind::Begin, l.span(&self.loc()))),
+            "end" => Ok(Token::new(TokenKind::End, l.span(&self.loc()))),
+            "match" => Ok(Token::new(TokenKind::Match, l.span(&self.loc()))),
+            "with" => Ok(Token::new(TokenKind::With, l.span(&self.loc()))),
+            "open" => Ok(Token::new(TokenKind::Open, l.span(&self.loc()))),
+            "do" => Ok(Token::new(TokenKind::Do, l.span(&self.loc()))),
+            "done" => Ok(Token::new(TokenKind::Done, l.span(&self.loc()))),
+            "to" => Ok(Token::new(TokenKind::To, l.span(&self.loc()))),
+            "let" => {
+                if self.is_operator_start(true) {
+                    let _ = self.lex_operator(session)?;
+                    let loc = self.loc();
+                    let s = &self.contents[l.offset..loc.offset];
+                    let symbol = session.intern_symbol(s);
+                    Ok(Token::new(TokenKind::LetOp(symbol), l.span(&loc)))
+                } else {
+                    Ok(Token::new(TokenKind::Let, l.span(&self.loc())))
+                }
+            }
+            _ => {
+                let symbol = session.intern_symbol(s);
+                Ok(Token::new(TokenKind::Ident(symbol), l.span(&self.loc())))
+            }
+        }
+    }
+
+    fn next_token(&mut self, session: &mut Session) -> LexRes {
+        self.skip_whitespace();
+        let l = self.loc();
+        if let Some(c) = self.peek() {
+            match c {
+                '(' => {
+                    if let Some('*') = self.peek_n(1) {
+                        self.lex_comment()
+                    } else {
+                        Ok(self.create_single_char_token(TokenKind::LPar))
+                    }
+                }
+                ')' => Ok(self.create_single_char_token(TokenKind::RPar)),
+                _ if self.is_operator_start(true) => {
+                    // This is an operator
+                    self.lex_operator(session)
+                }
+                '?' | '~' => todo!(),
+                _ if is_identifier_start(c) => self.lex_identifier(session),
+                _ => Err(LexingError::UnexpectedChar(l)),
+            }
+        } else {
+            Err(LexingError::EOF)
+        }
+    }
+}
+// ======================
+/// Iterator utilities
+// ======================
+
+pub struct SkipLexer<'session> {
+    session: &'session mut Session,
+    inner: Lexer<'session>,
+}
+
+pub struct LexerIter<'session> {
+    session: &'session mut Session,
+    inner: Lexer<'session>,
+}
+
+impl<'session> Iterator for LexerIter<'session> {
+    type Item = Token;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.inner.next_token(self.session).ok()
+    }
+}
+
+impl<'session> Iterator for SkipLexer<'session> {
+    type Item = Token;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        while let Ok(tok) = self.inner.next_token(self.session) {
+            if !tok.is_skip() {
+                return Some(tok);
+            }
+        }
+
+        None
+    }
+}
+
+impl<'session> Lexer<'session> {
+    pub fn no_skip(self, session: &'session mut Session) -> LexerIter<'session> {
+        LexerIter {
+            inner: self,
+            session,
+        }
+    }
+    pub fn skip(self, session: &'session mut Session) -> SkipLexer<'session> {
+        SkipLexer {
+            inner: self,
+            session,
+        }
+    }
 }
--- src/lexer/token.rs
+++ src/lexer/token.rs
@@ -1,11 +1,14 @@
+use std::fmt;
+
 use crate::{
     lexer::interner::{StrLit, Symbol},
+    session::Session,
     source_manager::loc::Span,
 };
 
 pub enum TokenKind {
     Ident(Symbol),
-    Intlit(u64),
+    Intlit(i64),
     Strlit(StrLit),
 
     True,
@@ -22,17 +25,30 @@ pub enum TokenKind {
     LetOp(Symbol),
     Rec,
     Fun,
+    For,
+    While,
+    Open,
+    Do,
+    Done,
+    To,
 
     Plus,
     Minus,
     Star,
     Div,
-    Dif,
     Eq,
-
+    Exclam,
+    NEq,
+    LT,
+    GT,
+    GEq,
+    LEq,
+    LOr,
+    Cons,
+    Pipe,
+    Arrow,
+    Pound,
     Op(Symbol),
-
-    Bar,
 
     Semi,
     Comma,
@@ -44,9 +60,259 @@ pub enum TokenKind {
     RBra,
     LSQr,
     RSQr,
+
+    Comment(String),
+}
+
+impl TokenKind {
+    pub fn is_comment(&self) -> bool {
+        match self {
+            Self::Comment(_) => true,
+            _ => false,
+        }
+    }
+
+    pub fn is_skip(&self) -> bool {
+        self.is_comment()
+    }
+
+    pub fn display<'a, 'b>(&'a self, sm: &'b Session) -> TokenKindDisplay<'a, 'b> {
+        TokenKindDisplay(self, sm)
+    }
+
+    pub fn debug<'a, 'b>(&'a self, sm: &'b Session) -> DebugTokenKindDisplay<'a, 'b> {
+        DebugTokenKindDisplay(self, sm)
+    }
 }
 
 pub struct Token {
     pub kind: TokenKind,
     pub span: Span,
+}
+
+pub struct TokenKindDisplay<'a, 'b>(&'a TokenKind, &'b Session);
+pub struct TokenDisplay<'a, 'b>(&'a Token, &'b Session);
+pub struct DebugTokenKindDisplay<'a, 'b>(&'a TokenKind, &'b Session);
+pub struct DebugTokenDisplay<'a, 'b>(&'a Token, &'b Session);
+
+impl<'a, 'b> fmt::Display for TokenDisplay<'a, 'b> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{}: {}",
+            self.0.span.display(&self.1.source_manager),
+            self.0.kind.display(self.1)
+        )
+    }
+}
+
+impl<'a, 'b> fmt::Display for DebugTokenDisplay<'a, 'b> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{}: {}",
+            self.0.span.display(&self.1.source_manager),
+            self.0.kind.debug(self.1)
+        )
+    }
+}
+
+impl<'a, 'b> fmt::Display for DebugTokenKindDisplay<'a, 'b> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let s = &self.1;
+
+        match self.0 {
+            TokenKind::Ident(_) => write!(f, "Ident: ")?,
+            TokenKind::Intlit(_) => write!(f, "Intlit: ")?,
+            TokenKind::Strlit(_) => write!(f, "Strlit: ")?,
+            TokenKind::LetOp(_) => write!(f, "LetOp: ")?,
+            TokenKind::Op(_) => write!(f, "Op: ")?,
+            TokenKind::Comment(_) => write!(f, "Comment: ")?,
+            TokenKind::True => write!(f, "True: ")?,
+            TokenKind::False => write!(f, "False: ")?,
+            TokenKind::If => write!(f, "If: ")?,
+            TokenKind::Then => write!(f, "Then: ")?,
+            TokenKind::Else => write!(f, "Else: ")?,
+            TokenKind::Match => write!(f, "Match: ")?,
+            TokenKind::With => write!(f, "With: ")?,
+            TokenKind::Begin => write!(f, "Begin: ")?,
+            TokenKind::End => write!(f, "End: ")?,
+            TokenKind::Let => write!(f, "Let: ")?,
+            TokenKind::Rec => write!(f, "Rec: ")?,
+            TokenKind::Fun => write!(f, "Fun: ")?,
+            TokenKind::For => write!(f, "For: ")?,
+            TokenKind::While => write!(f, "While: ")?,
+            TokenKind::Open => write!(f, "Open: ")?,
+            TokenKind::Do => write!(f, "Do: ")?,
+            TokenKind::Done => write!(f, "Done: ")?,
+            TokenKind::To => write!(f, "To: ")?,
+            TokenKind::Plus => write!(f, "Plus: ")?,
+            TokenKind::Minus => write!(f, "Minus: ")?,
+            TokenKind::Star => write!(f, "Star: ")?,
+            TokenKind::Div => write!(f, "Div: ")?,
+            TokenKind::Eq => write!(f, "Eq: ")?,
+            TokenKind::Exclam => write!(f, "Exclam: ")?,
+            TokenKind::NEq => write!(f, "NEq: ")?,
+            TokenKind::LT => write!(f, "LT: ")?,
+            TokenKind::GT => write!(f, "GT: ")?,
+            TokenKind::GEq => write!(f, "GEq: ")?,
+            TokenKind::LEq => write!(f, "LEq: ")?,
+            TokenKind::LOr => write!(f, "LOr: ")?,
+            TokenKind::Cons => write!(f, "Cons: ")?,
+            TokenKind::Pipe => write!(f, "Pipe: ")?,
+            TokenKind::Arrow => write!(f, "Arrow: ")?,
+            TokenKind::Pound => write!(f, "Pound: ")?,
+            TokenKind::Semi => write!(f, "Semi: ")?,
+            TokenKind::Comma => write!(f, "Comma: ")?,
+            TokenKind::Dot => write!(f, "Dot: ")?,
+            TokenKind::LPar => write!(f, "LPar: ")?,
+            TokenKind::RPar => write!(f, "RPar: ")?,
+            TokenKind::LBra => write!(f, "LBra: ")?,
+            TokenKind::RBra => write!(f, "RBra: ")?,
+            TokenKind::LSQr => write!(f, "LSQr: ")?,
+            TokenKind::RSQr => write!(f, "RSQr: ")?,
+        };
+
+        match self.0 {
+            TokenKind::Op(symbol) => {
+                write!(f, "{}", s.resolve_symbol(*symbol))
+            }
+            TokenKind::Ident(symbol) => {
+                write!(f, "{}", s.resolve_symbol(*symbol))
+            }
+            TokenKind::Intlit(x) => write!(f, "{x}"),
+            TokenKind::Strlit(str_lit) => {
+                let lit = s.resolve_strlit(*str_lit);
+                write!(f, "\"{}\"", lit.escape_debug())
+            }
+            TokenKind::True => write!(f, "true"),
+            TokenKind::False => write!(f, "false"),
+            TokenKind::If => write!(f, "if"),
+            TokenKind::Then => write!(f, "then"),
+            TokenKind::Else => write!(f, "else"),
+            TokenKind::Match => write!(f, "match"),
+            TokenKind::With => write!(f, "with"),
+            TokenKind::Begin => write!(f, "begin"),
+            TokenKind::End => write!(f, "end"),
+            TokenKind::Let => write!(f, "let"),
+            TokenKind::LetOp(symbol) => write!(f, "let{}", s.resolve_symbol(*symbol)),
+            TokenKind::Rec => write!(f, "rec"),
+            TokenKind::Fun => write!(f, "fun"),
+            TokenKind::For => write!(f, "for"),
+            TokenKind::While => write!(f, "while"),
+            TokenKind::Open => write!(f, "open"),
+            TokenKind::Do => write!(f, "do"),
+            TokenKind::Done => write!(f, "done"),
+            TokenKind::To => write!(f, "to"),
+            TokenKind::Plus => write!(f, "+"),
+            TokenKind::Minus => write!(f, "-"),
+            TokenKind::Star => write!(f, "*"),
+            TokenKind::Div => write!(f, "/"),
+            TokenKind::Eq => write!(f, "="),
+            TokenKind::Exclam => write!(f, "!"),
+            TokenKind::NEq => write!(f, "<>"),
+            TokenKind::LT => write!(f, "<"),
+            TokenKind::GT => write!(f, ">"),
+            TokenKind::GEq => write!(f, ">="),
+            TokenKind::LEq => write!(f, "<="),
+            TokenKind::LOr => write!(f, "||"),
+            TokenKind::Cons => write!(f, "::"),
+            TokenKind::Pipe => write!(f, "|"),
+            TokenKind::Arrow => write!(f, "->"),
+            TokenKind::Pound => write!(f, "&"),
+            TokenKind::Semi => write!(f, ";"),
+            TokenKind::Comma => write!(f, ","),
+            TokenKind::Dot => write!(f, "."),
+            TokenKind::LPar => write!(f, "("),
+            TokenKind::RPar => write!(f, ")"),
+            TokenKind::LBra => write!(f, "{{"),
+            TokenKind::RBra => write!(f, "}}"),
+            TokenKind::LSQr => write!(f, "["),
+            TokenKind::RSQr => write!(f, "]"),
+            TokenKind::Comment(s) => write!(f, "{}", s.escape_debug()),
+        }
+    }
+}
+
+impl<'a, 'b> fmt::Display for TokenKindDisplay<'a, 'b> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let s = &self.1;
+        match self.0 {
+            TokenKind::Op(symbol) | TokenKind::Ident(symbol) => {
+                write!(f, "{}", s.resolve_symbol(*symbol))
+            }
+            TokenKind::Intlit(x) => write!(f, "{x}"),
+            TokenKind::Strlit(str_lit) => {
+                let lit = s.resolve_strlit(*str_lit);
+                write!(f, "\"{}\"", lit.escape_debug())
+            }
+            TokenKind::True => write!(f, "true"),
+            TokenKind::False => write!(f, "false"),
+            TokenKind::If => write!(f, "if"),
+            TokenKind::Then => write!(f, "then"),
+            TokenKind::Else => write!(f, "else"),
+            TokenKind::Match => write!(f, "match"),
+            TokenKind::With => write!(f, "with"),
+            TokenKind::Begin => write!(f, "begin"),
+            TokenKind::End => write!(f, "end"),
+            TokenKind::Let => write!(f, "let"),
+            TokenKind::LetOp(symbol) => write!(f, "let{}", s.resolve_symbol(*symbol)),
+            TokenKind::Rec => write!(f, "rec"),
+            TokenKind::Fun => write!(f, "fun"),
+            TokenKind::For => write!(f, "for"),
+            TokenKind::While => write!(f, "while"),
+            TokenKind::Open => write!(f, "open"),
+            TokenKind::Do => write!(f, "do"),
+            TokenKind::Done => write!(f, "done"),
+            TokenKind::To => write!(f, "to"),
+            TokenKind::Plus => write!(f, "+"),
+            TokenKind::Minus => write!(f, "-"),
+            TokenKind::Star => write!(f, "*"),
+            TokenKind::Div => write!(f, "/"),
+            TokenKind::Eq => write!(f, "="),
+            TokenKind::Exclam => write!(f, "!"),
+            TokenKind::NEq => write!(f, "<>"),
+            TokenKind::LT => write!(f, "<"),
+            TokenKind::GT => write!(f, ">"),
+            TokenKind::GEq => write!(f, ">="),
+            TokenKind::LEq => write!(f, "<="),
+            TokenKind::LOr => write!(f, "||"),
+            TokenKind::Cons => write!(f, "::"),
+            TokenKind::Pipe => write!(f, "|"),
+            TokenKind::Arrow => write!(f, "->"),
+            TokenKind::Pound => write!(f, "&"),
+            TokenKind::Semi => write!(f, ";"),
+            TokenKind::Comma => write!(f, ","),
+            TokenKind::Dot => write!(f, "."),
+            TokenKind::LPar => write!(f, "("),
+            TokenKind::RPar => write!(f, ")"),
+            TokenKind::LBra => write!(f, "{{"),
+            TokenKind::RBra => write!(f, "}}"),
+            TokenKind::LSQr => write!(f, "["),
+            TokenKind::RSQr => write!(f, "]"),
+            TokenKind::Comment(s) => write!(f, "{}", s.escape_debug()),
+        }
+    }
+}
+
+impl Token {
+    pub fn new(kind: TokenKind, span: Span) -> Self {
+        Self { kind, span }
+    }
+
+    pub fn is_comment(&self) -> bool {
+        self.kind.is_comment()
+    }
+
+    pub fn is_skip(&self) -> bool {
+        self.kind.is_skip()
+    }
+
+    pub fn display<'a, 'b>(&'a self, sm: &'b Session) -> TokenDisplay<'a, 'b> {
+        TokenDisplay(self, sm)
+    }
+
+    pub fn debug<'a, 'b>(&'a self, sm: &'b Session) -> DebugTokenDisplay<'a, 'b> {
+        DebugTokenDisplay(self, sm)
+    }
 }
