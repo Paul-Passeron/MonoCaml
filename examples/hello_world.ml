1c82988f8277b7e9161a312e72323a2d7bacb5ab
Paul-Passeron <paul.passeron2@gmail.com>
2026-01-27 20:09:47 +0100

    Fix list reversal function implementation

--- examples/hello_world.ml
+++ examples/hello_world.ml
@@ -1,4 +1,6 @@
 let _ = print_endline "Hello, World!"
-let rec rev l = match l with
-    | a :: b -> (rev b) @ a
+let rec rev (l : 'abcd list): 'abcd list = match l with
+    | a :: b -> (rev b) @ [a]
     | [] -> []
+let my_char = 'c'
+let my_char = '\n'
