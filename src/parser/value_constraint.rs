d1752aa4fffde944bb05cd4fdfd7af47e86ee3b4
Paul-Passeron <paul.passeron2@gmail.com>
2026-02-03 11:35:48 +0100

    Added value constraint parsing (x: t)

--- src/parser/value_constraint.rs
+++ src/parser/value_constraint.rs
@@ -1,10 +1,16 @@
 use crate::{
+    lexer::token::TokenKind,
     parse_tree::expression::ValueConstraint,
     parser::{Parser, error::ParseRes},
 };
 
 impl<'a> Parser<'a> {
     pub fn parse_value_constraint(&mut self) -> ParseRes<ValueConstraint> {
-        todo!()
+        self.expect(TokenKind::Colon)?;
+        let typ = self.parse_type_expr()?;
+        Ok(ValueConstraint {
+            locally_abstract_types: vec![],
+            typ,
+        })
     }
 }
