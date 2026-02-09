de56563ae51e1e08c82d897929071a38e2195d6f
Paul-Passeron <paul.passeron2@gmail.com>
2026-02-09 12:42:28 +0100

    Add structure constructs to poly IR

--- src/resolved/poly_ir/structure.rs
+++ src/resolved/poly_ir/structure.rs
@@ -1,1 +1,17 @@
-pub struct PolyStruct {}
+use crate::{
+    resolved::poly_ir::expr::{Expr, LetBinding},
+    source_manager::loc::Span,
+};
+
+pub enum PolyStructKind {
+    Eval(Expr),
+    Decl {
+        rec: bool,
+        bindings: Vec<LetBinding>,
+    },
+}
+
+pub struct PolyStruct {
+    pub kind: PolyStructKind,
+    pub span: Span,
+}
