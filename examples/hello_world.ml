bb3c74e4c170cbdb68f92c34702c04c2f2b818f3
Paul-Passeron <paul.passeron2@gmail.com>
2026-02-24 11:24:58 +0100

    Add actual hello world in hello_world.ml example

--- examples/hello_world.ml
+++ examples/hello_world.ml
@@ -1,6 +1,1 @@
-let _ = print_endline "Hello, World!"
-let rec rev (l : 'abcd list): 'abcd list = match l with
-    | a :: b -> (rev b) @ [a]
-    | [] -> []
-let my_char = 'c'
-let my_char = '\n'
+let x = print_endline "hello, world !"
