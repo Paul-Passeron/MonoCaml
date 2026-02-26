8ae689baed4eb68ca8312fd1a968347dc3c7c27d
Paul-Passeron <paul.passeron2@gmail.com>
2026-02-26 19:13:15 +0100

    Made simple_poly.ml example more complex with map function on list and test cases.

--- examples/simple_poly.ml
+++ examples/simple_poly.ml
@@ -1,5 +1,10 @@
-let iden x = x
+let rec map f l = match l with
+    | [] -> []
+    | hd :: tl -> (f hd) :: map f tl
 
-let two = iden 2
+let mapped = map (fun x -> x + 1)
+let mapped_list = mapped [1; 2; 3]
 
-let hello = iden "hello"
+let helloed_lst1 = map (fun x -> "hello") ["a"; "b"; "c"]
+let helloed_lst2 = map (fun x -> "hello") [1; 2; 3]
+let final = map (fun x -> 2) (map (fun x -> "hello") [1; 2; 3])
